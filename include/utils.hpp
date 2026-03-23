b39b63e67bcec1ffb86c05c42e298d1357d6a095
Viktor Griniushin <v.griniushin@gmail.com>
2026-03-23 14:43:20 +0100

    chore(repo): remove raw root files from remote tracking

--- include/utils.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <vector>
-#include <TTree.h>
-#include <TString.h>
-
-std::vector<Int_t> numberOfHitsPerEvent(TTree*, const TString&, bool);
-std::vector<Int_t> mapChannelToLayer(TTree*, const TString&, bool);
