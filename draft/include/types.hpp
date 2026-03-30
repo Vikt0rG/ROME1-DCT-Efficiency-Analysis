c36124f80d04df6fd4de2ad85ac57a600292c5ba
Viktor Griniushin <v.griniushin@gmail.com>
2026-03-30 16:04:41 +0200

    feat(utils): add core types and utility helpers

---
+++ draft/include/types.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+struct EfficiencyCounters {
+    int triggered_events_external;
+    int triggered_events_rpc[3];
+    
+    int eta1_efficiency_counter[3];
+    int eta2_efficiency_counter[3];
+    int eta_or_efficiency_counter[3];
+    int eta_and_efficiency_counter[3];
+    
+    int eta1_efficiency_counter_rpc[3];
+    int eta2_efficiency_counter_rpc[3];
+    int eta_or_efficiency_counter_rpc[3];
+    int eta_and_efficiency_counter_rpc[3];
+};
