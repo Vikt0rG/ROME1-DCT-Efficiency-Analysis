55f0fb5c9d363b1271d9b4f9d613f306b920976b
Viktor Griniushin <v.griniushin@gmail.com>
2026-03-30 16:04:41 +0200

    Adding types and utils.

---
+++ draft/utils/utils.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+// Utility functions for time conversion and other time related operations
+namespace TimeUtils {
+    static int convertRawTimeToPhysical(int raw_time, int bcid) {
+        return raw_time != 0 ? bcid * 30 + raw_time - 1 : -1;
+    }
+}
